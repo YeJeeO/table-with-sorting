b6e0682c3bda67ffaa1e3ede1ac010e99fd35a80
YeJeeO <yejeeo@yandex.ru>
2023-04-01 12:31:18 +0700

    modified:   index.js 	modified:   pages/_app.js 	deleted:    style.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../pages/style.css';
+import Table from '../components/Table';
 
 
 export default function MyApp({ Component, pageProps }) {
   return <><Component {...pageProps} />
+  <Table/>
    </>;
 }
